efe7de324fb71f9d83db680a560434003805c049
Spencer Lazzar <spencerlazzar@yahoo.com>
2020-08-06 16:18:01 -0700

    Final Push with Functions

--- day02/demo/js/app.js
+++ day02/demo/js/app.js
@@ -1,31 +1,28 @@
 'use strict';
 
 
-// function getUserName() {
-//   var userName = prompt('Hi, What is your name?');
-//   console.log('the user\'s name is' + userName);
-//   alert('Hi ' + userName + '!' + ' Welcome to my site!');
-//   alert('Please read my bio below and take a short quiz');
-//   document.write('<h2>' + 'Hi ' + userName + '</h2>');
-//   document.write('Here are some fun facts about me!');
-// }
+function getUserName() {
+  var userName = prompt('Hi, What is your name?');
+  console.log('the user\'s name is' + userName);
+  alert('Hi ' + userName + '!' + ' Welcome to my site!');
+  alert('Please read my bio below and take a short quiz');
+  document.write('<h2>' + 'Hi ' + userName + '</h2>');
+  document.write('Here are some fun facts about me!');
+}
+getUserName();
 
-// getUserName();
-
-
-// function whereFrom() {
-//   var questionOne = prompt('Where am I originally from?');
-//   // console.log(questionOne, 'test');
-//   if (questionOne.toLowerCase() === 'salt lake city') {
-//     alert('CORRECT!');
-//     document.write('<li>For question one you answered ' + questionOne + '. CORRECT!</li>');
-//   } else {
-//     alert('Nope, that is incorrect');
-//     document.write('<li>For question one you answered ' + questionOne + '. that was incorrect</li>');
-//   }
-// }
-
-// whereFrom();
+function whereFrom() {
+  var questionOne = prompt('Where am I originally from?');
+  // console.log(questionOne, 'test');
+  if (questionOne.toLowerCase() === 'salt lake city') {
+    alert('CORRECT!');
+    document.write('<li>For question one you answered ' + questionOne + '. CORRECT!</li>');
+  } else {
+    alert('Nope, that is incorrect');
+    document.write('<li>For question one you answered ' + questionOne + '. that was incorrect</li>');
+  }
+}
+whereFrom();
 
 function degree() {
   var questionTwo = prompt('What is my degree in?');
@@ -38,73 +35,89 @@ function degree() {
     document.write('<li>For question one you answered ' + questionTwo + '. that was incorrect</li>');
   }
 }
-
 degree();
 
+function studyGuitar() {
+  var questionThree = prompt('Who did I study guitar with?');
+  // console.log(questionOne, 'test');
+  if (questionThree.toLowerCase() === 'robert fripp') {
+    alert('CORRECT!');
+    document.write('<li>For question one you answered ' + questionThree + '. CORRECT!</li>');
+  } else {
+    alert('Nope, that is incorrect');
+    document.write('<li>For question one you answered ' + questionThree + '. that was incorrect</li>');
+  }
+}
+studyGuitar();
 
-// var questionThree = prompt('Who did I study guitar with?');
-// // console.log(questionOne, 'test');
-// if (questionThree.toLowerCase() === 'robert fripp'){
-//   alert('CORRECT!');
-//   document.write('<li>For question one you answered ' + questionThree + '. CORRECT!</li>');
-// } else {
-//   alert('Nope, that is incorrect');
-//   document.write('<li>For question one you answered ' + questionThree + '. that was incorrect</li>');
-// }
-// var questionFour = prompt('What kind of music do I play?');
-// // console.log(questionOne, 'test');
-// if (questionFour.toLowerCase() === 'jazz'){
-//   alert('CORRECT!');
-//   document.write('<li>For question one you answered ' + questionFour + '. CORRECT!</li>');
-// } else {
-//   alert('Nope, that is incorrect');
-//   document.write('<li>For question one you answered ' + questionFour + '. that was incorrect</li>');
-// }
-// var questionFive = prompt('What city do I play jazz?');
-// // console.log(questionOne, 'test');
-// if (questionFive.toLowerCase() === 'tacoma'){
-//   alert('CORRECT!');
-//   document.write('<li>For question one you answered ' + questionFive + '. CORRECT!</li>');
-// } else {
-//   alert('Nope, that is incorrect');
-//   document.write('<li>For question one you answered ' + questionFive + '. that was incorrect</li>');
-// }
+function kindMusic() {
+  var questionFour = prompt('What kind of music do I play?');
+  // console.log(questionOne, 'test');
+  if (questionFour.toLowerCase() === 'jazz') {
+    alert('CORRECT!');
+    document.write('<li>For question one you answered ' + questionFour + '. CORRECT!</li>');
+  } else {
+    alert('Nope, that is incorrect');
+    document.write('<li>For question one you answered ' + questionFour + '. that was incorrect</li>');
+  }
+}
+kindMusic();
 
-// for (var i=0; i<4; i++){
-//   var questionSix = prompt('Can you guess my favorite number between 1-10?');
-//   questionSix = parseInt(questionSix);
-//   console.log (questionSix);
-//   if (questionSix === 8){
-//     console.log ('correct');
-//     alert ('You are Correct!');
-//     break;
+function cityJazz() {
+  var questionFive = prompt('What city do I play jazz?');
+  // console.log(questionOne, 'test');
+  if (questionFive.toLowerCase() === 'tacoma') {
+    alert('CORRECT!');
+    document.write('<li>For question one you answered ' + questionFive + '. CORRECT!</li>');
+  } else {
+    alert('Nope, that is incorrect');
+    document.write('<li>For question one you answered ' + questionFive + '. that was incorrect</li>');
+  }
+}
+cityJazz();
 
-//   }else if (questionSix > 8){
-//     console.log (questionSix);
-//     alert ('You are too high');
+function guessNum() {
+  for (var i = 0; i < 4; i++) {
+    var questionSix = prompt('Can you guess my favorite number between 1-10?');
+    questionSix = parseInt(questionSix);
+    console.log(questionSix);
+    if (questionSix === 8) {
+      console.log('correct');
+      alert('You are Correct!');
+      break;
 
-//   }else if (questionSix < 8){
-//     console.log (questionSix);
-//     alert ('You are too low');
-//   }
-//   if (i===3){
-//     alert ('You are out of guesses, the answer is 8!');
-//   }
+    } else if (questionSix > 8) {
+      console.log(questionSix);
+      alert('You are too high');
 
-// }
-// var favMovies = ['waiting for guffman', 'pale rider', 'death proof','the jerk','swingers','zelig','superbad','sean of the dead','rubin and ed','napoleon dynamite'];
-// // console.log ('line 78',favMovies[4])
-// var favorite = 'the jerk';
-// console.log ('question seven', userResponse)
-// for (var i=0; i<6; i++){
-//   console.log ('i-', i)
-//   console.log ('favMovies at i-', favMovies[i])
-//   var userResponse = prompt('Can you guess my favorite movie?');
-//   if (userResponse.toLowerCase() === favorite){
-//     alert ('You are correct!');
-//     break;    
-//   }
-// }
-//     alert ('Sorry, You ran out of guesses.');
+    } else if (questionSix < 8) {
+      console.log(questionSix);
+      alert('You are too low');
+    }
+    if (i === 3) {
+      alert('You are out of guesses, the answer is 8!');
+    }
+  }
+}
+guessNum();
 
-// document.write('Thanks ' + userName + '!' + ' for visiting and taking the quiz!');
+
+
+var favMovies = ['waiting for guffman', 'pale rider', 'death proof', 'the jerk', 'swingers', 'zelig', 'superbad', 'sean of the dead', 'rubin and ed', 'napoleon dynamite'];
+// console.log ('line 78',favMovies[4])
+var attempts = 6;
+function final() {
+  for (var i = 0; i < attempts; i++) {
+    var userResponse = prompt('Can you guess my favorite movie?').toLowerCase();
+    if (userResponse === favMovies[0] || userResponse === favMovies[1] || userResponse === favMovies[2] || userResponse === favMovies[3] || userResponse === favMovies[4] || userResponse === favMovies[5] || userResponse === favMovies[6] || userResponse === favMovies[7] || userResponse === favMovies[8] || userResponse === favMovies[9] || userResponse === favMovies[10]) {
+      alert('You are correct!');
+      break;
+    }
+    else if (userResponse !== favMovies) {
+      alert('You are incorrect')
+    }
+  }
+}
+final();
+
+document.write('Thanks ' + userName + '!' + ' for visiting and taking the quiz!');
